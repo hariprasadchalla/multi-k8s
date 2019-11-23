72e1bfd9e5cc645993a4eec54e5e73a776f68c58
challa <prasadchalla88@gmail.com>
2019-11-23 16:57:27 +0530

    chnage push to own repo

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t stephengrider/multi-client:latest -t harichalla/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server:latest -t harichalla/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t harichalla/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push harichalla/multi-client:latest
+docker push harichalla/multi-server:latest
+docker push harichalla/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push harichalla/multi-client:$SHA
+docker push harichalla/multi-server:$SHA
+docker push harichalla/multi-worker:$SHA
 
 
 kubectl apply -f k8s
